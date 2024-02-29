0449aa2cfebf390467a5cfcde775604deb5e40c4
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-02-29 23:08:03 +0900

    kotlin project

---
+++ src/main/kotlin/Type.kt
@@ -0,0 +1,12 @@
+package org.example
+
+fun main() {
+    val playerName: String = "Estragon"
+    var experiencePoint: Int = 5
+    experiencePoint += 5
+    var hasSteed: Boolean = false
+    val drinks : List<String> = listOf("mead", "wine", "LaCroix")
+
+    println(experiencePoint)
+    println(playerName.reversed())
+}
