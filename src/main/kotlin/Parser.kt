ce091f24a2c6fa1bc04accafb896991bb834a5e1
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-03-03 23:11:41 +0900

    a instruction encode

---
+++ src/main/kotlin/Parser.kt
@@ -0,0 +1,25 @@
+package org.example
+import java.io.File
+
+class Parser {
+    public fun readline(filePath: String): List<String> {
+        val linesWithoutComments = File(filePath)
+            .readLines()
+            .map { it.trim() }
+            .filter { skipCommentsAndBlankLine(it) }
+            .map { skipInlineCommentsAndWhiteSpace(it) }
+        return linesWithoutComments
+    }
+
+    private fun skipCommentsAndBlankLine(line: String): Boolean {
+        return line.isNotEmpty() && !line.startsWith("//")
+    }
+
+    private fun skipInlineCommentsAndWhiteSpace(line: String): String {
+        if (line.contains("//")) {
+            val startIndex = line.indexOf("//")
+            return line.substring(0, startIndex).trim()
+        }
+        return line
+    }
+}
