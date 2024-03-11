ed4f9477e3dc464623f6340f938b9a1fd498df5d
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-03-12 01:44:44 +0900

    提出用ファイル作成

--- src/main/kotlin/VMTranslator/MemorySegment.kt
+++ src/main/kotlin/VMTranslator/MemorySegment.kt
@@ -1,21 +1,29 @@
 package org.example.VMTranslator
 
-class MemorySegment {
+class MemorySegment(className:String) {
 
     private val memorySegmentName = mapOf("local" to "LCL", "argument" to "ARG", "this" to "THIS", "that" to "THAT")
-
-    fun setMemoryDataToDRegister(segment:String, address: Int):String = when (segment) {
-            "constant" -> setDataRegisterAtConstantAddress(address)
-            "static" -> ""
-            "pointer" -> ""
-            "temp" -> ""
+    val className = className
+    fun setDToRAMAt(segment:String, address: Int):String = when (segment) {
+            "constant" -> setDataRegisterToConstant(address)
+            "static" -> setDRegisterToStaticMemory(address)
+            "pointer" -> setDRegisterToPointer(address)
+            "temp" -> setDataRegisterToTemp(address)
             else -> setDRegisterToMemoryAtLocalOrArgumentOrThisOrThat(segment, address)
         }
-    fun setDataRegisterAddress(segment:String, address: Int):String = when (segment) {
-        "constant" -> setDataRegisterAtConstantAddress(address)
-        "static" -> ""
-        "pointer" -> ""
-        "temp" -> ""
+
+    fun addDtoAddress(segment:String, address: Int):String = when (segment) {
+        "constant" -> addDtoConstantAddress(address)
+        "static" -> addDtoStaticAddress(address)
+        "pointer" -> addDtoPointerAddress(address)
+        "temp" -> addDtoTempAddress(address)
+        else -> addDtoLocalOrArgumentOrThisOrThatAddress(segment, address)
+    }
+    fun setDToAddress(segment:String, address: Int):String = when (segment) {
+        "constant" -> setDataRegisterToConstant(address)
+        "static" -> setDRegisterToStaticAddress(address)
+        "pointer" -> setDRegisterToPointerAddress(address)
+        "temp" -> setDataRegisterToTempAddress(address)
         else -> setDataRegisterAtLocalOrArgumentOrThisOrThat(segment, address)
     }
 
@@ -31,7 +39,52 @@ class MemorySegment {
         return memoryAssembly
     }
 
-    fun setDataRegisterAtConstantAddress(address: Int):String {
+    private fun addDtoLocalOrArgumentOrThisOrThatAddress(segment:String, address: Int):String {
+        val segmentAssemblyName = memorySegmentName[segment] ?: throw IllegalStateException("不正なメモリーセグメントが指定されています")
+        var memoryAssembly = "@$segmentAssemblyName\nD=D+M\n@$address\nD=D+A\n"
+        return memoryAssembly
+    }
+
+    fun setDataRegisterToConstant(address: Int):String {
         return "@$address\nD=A\n"
+    }
+
+    fun addDtoConstantAddress(address: Int):String {
+        return "@$address\nD=D+A\n"
+    }
+
+    fun setDataRegisterToTemp(address: Int):String {
+        return "@${address+5}\nD=M\n"
+    }
+    fun addDtoTempAddress(address: Int):String {
+        return "@${address+5}\nD=D+A\n"
+    }
+    fun setDataRegisterToTempAddress(address: Int):String {
+        return "@${address+5}\nD=A\n"
+    }
+
+    fun setDRegisterToStaticMemory(address: Int):String {
+        return "@$className.$address\nD=M\n"
+    }
+
+    fun setDRegisterToStaticAddress(address: Int):String {
+        return "@$className.$address\nD=A\n"
+    }
+
+    fun addDtoStaticAddress(address: Int):String {
+        return "@$className.$address\nD=D+A\n"
+    }
+
+    fun setDRegisterToPointer(address:Int):String {
+        val pointer = if (address==0) 3 else 4
+        return "@$pointer\nD=M\n"
+    }
+    fun addDtoPointerAddress(address:Int):String {
+        val pointer = if (address==0) 3 else 4
+        return "@$pointer\nD=D+A\n"
+    }
+    fun setDRegisterToPointerAddress(address:Int):String {
+        val pointer = if (address==0) 3 else 4
+        return "@$pointer\nD=A\n"
     }
 }
--- src/main/kotlin/VMTranslator/Pointer.kt
+++ src/main/kotlin/VMTranslator/Pointer.kt
@@ -1,10 +1,10 @@
 package org.example.VMTranslator
 
-class Pointer() {
-    val memorySegment = MemorySegment()
+class Pointer(className:String) {
+    val memorySegment = MemorySegment(className)
     fun codePush(seg:String, address:Int):String {
         var code = ""
-        code += memorySegment.setMemoryDataToDRegister(seg, address) //push元のアドレスをアドレスレジスタに入れる
+        code += memorySegment.setDToRAMAt(seg, address) //push元のアドレスをアドレスレジスタに入れる
         code += setMemorySP + "M=D\n" //Stackにpush元データを保存
         code += incrementSP
         return code
@@ -12,14 +12,12 @@ class Pointer() {
 
     fun codePop(seg:String, address:Int):String {
         var code = ""
-        code += decrementSP //popするStackの位置へ移動
-        code += memorySegment.setDataRegisterAddress(seg, address) //pop先アドレスをDに格納
-        code += "@pop\nM=D\n" //pop先アドレスを@popに保存
-        code += setMemorySP + "D=M\n" //popする数値をDに格納
-        code += "@pop\nA=M\nM=D\n" //popする数値を格納したDをpop先アドレスにうつす
-        code += incrementSP
+        code += decrementSP + "A=M\nD=M\n" //popするvalueをDに格納
+        code += memorySegment.addDtoAddress(seg, address) //Dにpop先のアドレスを格納(D=popするvalue + pop先.address)
+        code += setMemorySP + "A=M\n" //A=pop value
+        code += "A=D-A\n" //Aにpop先のアドレスを格納(D=popするvalue + pop先address)
+        code += "M=D-A\n" //M=popする値
         return code
-
     }
 
     val incrementSP = "@SP\nM=M+1\n"
