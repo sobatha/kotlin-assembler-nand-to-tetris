a7bee59a6c4fc19238eed7f195dcce0c1b4b3bc0
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-03-26 00:45:55 +0900

    read jack file and parse it into list of tokens

--- src/main/kotlin/Compiler/JackTokenizer.kt
+++ src/main/kotlin/Compiler/JackTokenizer.kt
@@ -1,8 +1,25 @@
 package Compiler
+
+import java.io.File
+
 val TOKEN_TYPE = listOf("KEYWORD", "SYMBOL", "IDENTIFIER", "INT_CONST", "STRING_CONST")
-val KEYWORDS = listOf("CLASS", "METHOD", "CONSTRUCTOR", "INT", "BOOLEAN", "CHAR", "VOID", "STATIC",
-                    "VAR", "FIELD", "LET", "DO", "IF", "ELSE", "WHILE", "RETURN", "TRUE", "FALSE", "NULL", "THIS")
-class JackTokenizer(fileName: String) {
+val KEYWORDS = listOf("class", "constructor", "function", "method", "field",  "static", "var", "int", "char",
+"boolean", "void", "true", "false", "null", "this", "let", "do", "if", "else", "while", "return")
+val SYMBOLS = listOf('{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~')
+class JackTokenizer(filePath: String) {
+
+    var token = ""
+    var currentTokenType = ""
+    private var commentFlag = false
+    var tokens:MutableList<String> = mutableListOf()
+
+    init {
+        val lines = File(filePath)
+            .readLines()
+            .filter { skipCommentsAndBlankLine(it) }
+            .map { skipInlineCommentsAndWhiteSpace(it) }
+        for (line in lines) decomposeToken(line)
+    }
     fun hasMoreToken():Boolean {
         return true
     }
@@ -34,4 +51,47 @@ class JackTokenizer(fileName: String) {
     fun getStringVal(): String {
         return "string"
     }
+    private fun skipCommentsAndBlankLine(line: String): Boolean {
+        var temp = line
+
+        if (temp.startsWith("/**")) {
+            commentFlag = true
+        }
+        if (commentFlag) {
+            if (temp.endsWith("*/")) {
+                commentFlag = false
+            }
+            temp = ""
+        }
+        return temp.isNotEmpty() && !temp.startsWith("//")
+    }
+
+    private fun skipInlineCommentsAndWhiteSpace(line: String): String {
+        if (line.contains("//")) {
+            val startIndex = line.indexOf("//")
+            return line.substring(0, startIndex).trim()
+        }
+        return line.trim()
+    }
+
+    private fun decomposeToken(line: String) {
+        val words = line.split(" ")
+        for (word in words) {
+            println(word)
+            var temp = word.trim()
+            var token = ""
+            for (c in temp) {
+                if (c in SYMBOLS) {
+                    if (token.isNotEmpty()) {
+                        tokens.add(token)
+                        token = ""
+                    }
+                    tokens.add(c.toString())
+                }
+                else token += c
+            }
+            if (token.isNotEmpty()) tokens.add(token)
+        }
+    }
+
 }
--- src/main/kotlin/Compiler/Main.kt
+++ src/main/kotlin/Compiler/Main.kt
@@ -1,4 +1,22 @@
-package Compiler
 
-class Main {
-}
+package Compiler
+ fun main() {
+     val file = "data/10/ExpressionLessSquare/Main.jack"
+
+     var tokenizer = JackTokenizer(file)
+     println(tokenizer.tokens)
+
+//     while (tokenizer.hasMoreToken()) {
+//         var token = when (tokenizer.getTokenType()) {
+//             "KEYWORD" -> tokenizer.getKeyWord()
+//             "SYMBOL" -> tokenizer.getSymbol()
+//             "IDENTIFIER" -> tokenizer.getIdentifier()
+//             "INT_CONST" -> tokenizer.getIntVal()
+//             "STRING_CONST" -> tokenizer.getStringVal()
+//             else -> "error"
+//         }
+//
+//         println("${tokenizer.getTokenType()} : $token")
+//     }
+
+ }
