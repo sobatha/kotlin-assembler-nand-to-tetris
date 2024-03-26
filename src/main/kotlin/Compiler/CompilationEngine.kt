2fdc33b4a0b82e24f535e2a50423d084f7d9f5c6
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-03-26 14:35:39 +0900

    outline compile engine class

---
+++ src/main/kotlin/Compiler/CompilationEngine.kt
@@ -0,0 +1,63 @@
+package Compiler
+
+class CompilationEngine(inputPath: String, outputPath: String) {
+    fun compileClass() {
+        TODO()
+    }
+
+    fun compileClassBarDec() {
+        TODO()
+    }
+
+    fun compileSubroutine() {
+        TODO()
+    }
+
+    fun compileParameterList() {
+        TODO()
+    }
+
+    fun compileSubroutineBody() {
+        TODO()
+    }
+
+    fun compileVarDec() {
+        TODO()
+    }
+
+    fun compileStatements() {
+        TODO()
+    }
+
+    fun compileLet() {
+        TODO()
+    }
+
+    fun compileIf() {
+        TODO()
+    }
+
+    fun compileWhile() {
+        TODO()
+    }
+
+    fun compileDo() {
+        TODO()
+    }
+
+    fun compileReturn() {
+        TODO()
+    }
+
+    fun compileExpression() {
+        TODO()
+    }
+
+    fun compileTerm() {
+        TODO()
+    }
+
+    fun compileExpressionList() {
+        TODO()
+    }
+}
