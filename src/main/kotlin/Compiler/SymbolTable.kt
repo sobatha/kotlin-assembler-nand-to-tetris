ef29eded47aabdd1b718e043fd10ccec18e864ea
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-04-02 00:22:15 +0900

    outline for project 11

---
+++ src/main/kotlin/Compiler/SymbolTable.kt
@@ -0,0 +1,41 @@
+package Compiler
+enum class Kind {
+    STATIC, FIELD, ARG, VAR
+}
+class SymbolTable {
+    var table = mutableMapOf<String, Symbol>()
+    val kindIndex = mutableMapOf<Kind, Int>(
+        Kind.VAR to 0, Kind.STATIC to 0, Kind.FIELD to 0, Kind.ARG to 0
+    )
+
+    val reset = {
+        table = mutableMapOf<String, Symbol>()
+    }
+    val define = { name: String, type:String, kind:Kind ->
+        val index = kindIndex[kind]!!
+        kindIndex[kind] = index + 1
+        val symbol = Symbol(name, type, kind, index)
+        table[name] = symbol
+    }
+    val varCount = { it:String ->
+        kindIndex.getOrDefault(kindOf(it), 0)
+    }
+    val kindOf = { it: String ->
+        when (it) {
+            "static" -> Kind.STATIC
+            "field" -> Kind.FIELD
+            "arg" -> Kind.ARG
+            "var" -> Kind.VAR
+            else -> IllegalStateException("ünknown identifier kind")
+        }
+    }
+    val typeOf = { name: String ->
+        table.getOrDefault(name, null)?.type
+    }
+    val indexOf = { name:String ->
+        table.getOrDefault(name, null)?.index
+    }
+}
+
+data class Symbol(val name: String, val type: String, val kind: Kind, val index:Int) {
+}
