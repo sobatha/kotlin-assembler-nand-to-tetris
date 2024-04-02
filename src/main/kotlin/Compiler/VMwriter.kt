0e5c109016a46d5017d53195db9da84a816c69da
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-04-02 14:31:20 +0900

    Vmwriter実装

--- src/main/kotlin/Compiler/VMwriter.kt
+++ src/main/kotlin/Compiler/VMwriter.kt
@@ -1,32 +1,34 @@
 package Compiler
 
+import java.io.File
+
 class VMwriter(val outputFilePath: String) {
     fun writePush(segment: String, index:Int) {
-
+        write("push ${segment.lowercase()} $index")
     }
-
     fun writePop(segment: String, index: Int) {
-
+        write("pop ${segment.lowercase()} $index")
     }
-
     fun writeArithmetic(command:String) {
-
+        write("${command.lowercase()}")
     }
     fun writeLabel(label: String) {
-
+        write("label $label")
     }
-
     fun writeGoto(label: String) {
-
+        write("goto $label")
     }
     fun writeIf(label: String) {
-
+        write("if-goto $label")
     }
     fun writeCall(name: String, nArgs: Int) {
-
+        write("call $name $nArgs")
     }
     fun writeFunction(name: String, nVars: Int) {
-
+        write("function $name $nVars")
     }
-    fun writeReturn() {}
+    fun writeReturn() { write("return") }
+    private fun write(content:String) {
+        File(outputFilePath).writeText(content + "\n")
+    }
 }
