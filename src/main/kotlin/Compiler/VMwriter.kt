aa22a782f7f26a3d44d0d8d09d7a72207f6a2fe8
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-04-02 14:07:22 +0900

    outline for project 11

--- src/main/kotlin/Compiler/VMwriter.kt
+++ src/main/kotlin/Compiler/VMwriter.kt
@@ -1,4 +1,32 @@
 package Compiler
 
-class VMwriter {
+class VMwriter(val outputFilePath: String) {
+    fun writePush(segment: String, index:Int) {
+
+    }
+
+    fun writePop(segment: String, index: Int) {
+
+    }
+
+    fun writeArithmetic(command:String) {
+
+    }
+    fun writeLabel(label: String) {
+
+    }
+
+    fun writeGoto(label: String) {
+
+    }
+    fun writeIf(label: String) {
+
+    }
+    fun writeCall(name: String, nArgs: Int) {
+
+    }
+    fun writeFunction(name: String, nVars: Int) {
+
+    }
+    fun writeReturn() {}
 }
