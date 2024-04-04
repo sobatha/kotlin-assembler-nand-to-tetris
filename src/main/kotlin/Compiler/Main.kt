de8973f7d160c2aa987d696bb1dc76e6d1933925
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-04-04 17:30:27 +0900

    test file until square game

--- src/main/kotlin/Compiler/Main.kt
+++ src/main/kotlin/Compiler/Main.kt
@@ -7,15 +7,13 @@ import org.example.VMTranslator.initSys
 import java.io.File
 
 fun main() {
-     val path = "data/11/Seven/Main.jack"
+     val path = "data/11/Square"
 
 
      if (path.endsWith(".jack")) {
          var tokenizer = JackTokenizer(path)
          var compiler = CompilationEngine(tokenizer, path.removeSuffix(".jack")+".vm")
          compiler.compile()
-         println("class symbol table: ")
-         compiler.classSymbolTable.printTable()
          return
      }
 
@@ -23,7 +21,7 @@ fun main() {
      File(path).walk().forEach {
          if (it.toString().endsWith(".jack")) {
              var tokenizer = JackTokenizer(it.toString())
-             var compiler = CompilationEngine(tokenizer, it.toString().removeSuffix(".jack")+"Test.xml")
+             var compiler = CompilationEngine(tokenizer, it.toString().removeSuffix(".jack")+".vm")
              compiler.compile()
          }
      }
